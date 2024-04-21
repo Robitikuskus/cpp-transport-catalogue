7a7debf9218a1947032543796a09b04ed4be0957
Robitikuskus <robitikuskus@gmail.com>
2024-04-21 16:20:19 +0500

    got rid of unnecessary structures

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,8 +1,31 @@
 #include "stat_reader.h"
-#include "geo.h"
-#include <vector>
 #include <string>
 #include <iostream>
+#include <iomanip>
+
+std::ostream& operator<<(std::ostream& os, const tc::RouteInfo& route) {
+    if (!route.is_found) {
+        return os << "not found";
+    }
+
+    os << route.stops_count << " stops on route, " << route.unique_stops_count
+        << " unique stops, " << std::setprecision(6)
+        << route.route_length << " route length";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const tc::Stop& stop) {
+    if (!stop.is_found)
+        return os << "not found";
+    if (!stop.is_not_empty)
+        return os << "no buses";
+
+    os << "buses ";
+    for (const auto& e : stop.routes) {
+        os << e << " ";
+    }
+    return os;
+}
 
 void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue,
                     std::string_view request, std::ostream& output) {
--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,41 +1,37 @@
 #include "transport_catalogue.h"
+#include <algorithm>
 #include <unordered_set>
 #include <stdexcept>
 #include <iostream>
 
-void TransportCatalogue::AddStop(const std::string& name, const Coordinates& coordinates) {
+void TransportCatalogue::AddStop(const std::string_view& name, const Coordinates& coordinates) {
     stops_.emplace_back(name, coordinates);
 }
 
-void TransportCatalogue::AddRoute(const std::string& name, const std::vector<std::string_view>& stops) {
+void TransportCatalogue::AddRoute(const std::string_view& name, const std::vector<std::string_view>& stops) {
     routes_.emplace_back(tc::Route());
     routes_.back().name = name;
     routes_.back().stops_count = stops.size();
 
-    routes_.back().stops.reserve(stops.size());
-    for (size_t i = 0; i < stops.size(); ++i) {
-        auto s = std::find(stops_.begin(), stops_.end(), stops[i]);
-        s->routes.emplace(routes_.back().name);
-        routes_.back().stops[i] = &(*s);
-    }
     for (size_t i = 0; i < stops.size() - 1; ++i) {
-        routes_.back().distance += ComputeDistance(
-            routes_.back().stops[i]->coordinates,
-            routes_.back().stops[i + 1]->coordinates);
+        auto current_stop = std::find(stops_.begin(), stops_.end(), stops[i]);
+        auto next_stop = std::find(stops_.begin(), stops_.end(), stops[i + 1]);
+        routes_.back().distance += ComputeDistance(current_stop->coordinates, next_stop->coordinates);
+        current_stop->routes.emplace(routes_.back().name);
     }
 
     routes_.back().unique_stops_count = std::unordered_set<std::string_view>(
         std::move_iterator(stops.begin()), std::move_iterator(stops.end())).size();
 }
 
-tc::response::Route TransportCatalogue::OutRoute(const std::string_view& name) const {
+tc::RouteInfo TransportCatalogue::OutRoute(const std::string_view& name) const {
     auto route = std::find(routes_.begin(), routes_.end(), name);
     if (route == routes_.end())
         return {false};
     return {true, route->stops_count, route->unique_stops_count, route->distance};
 }
 
-tc::response::Stop TransportCatalogue::OutStop(const std::string_view& name) const {
+tc::Stop TransportCatalogue::OutStop(const std::string_view& name) const {
     auto stop = std::find(stops_.begin(), stops_.end(), name);
     if (stop == stops_.end())
         return {false};
